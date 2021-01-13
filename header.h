f5bbd90ff6a3bc38d00f0ac6e2478d5c47900efd
calmsacibis995 <46319359+calmsacibis995@users.noreply.github.com>
2021-01-13 20:31:06 +0200

    Add files via upload

---
+++ header.h
@@ -0,0 +1,2 @@
+#define MYNAME "ZeroCool32"
+#define HMSAGE 120000
---
+++ hmsage.c
@@ -0,0 +1,9 @@
+#include <header.h>
+
+int main()
+{
+	int hmsAge = (AGE * 4) + 340 - 2000;
+	printf("%s ' hms age is %d", MYNAME, hmsAge);
+	
+	return 0;
+}
